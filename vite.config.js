e00776bd37b88b2d742c9810b96524d56dd2edfd
ryckie wong <ricky600900@gmail.com>
2025-10-25 23:13:23 +0800

    更新 vite.config.js 設定

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: "/https://github.com/chuvivi/guodong-shop.git/",
+  base: "/guodong-shop/",
   plugins: [
     liveReload([
       "./layout/**/*.ejs",
